53496a21eab71b0fb8dac001c02178a7d967b0f9
DeveloperSky95 <haneulgim95@gmail.com>
2026-06-29 14:34:50 +0900

    AdminHeaderMobile 1차 작성 완료

---
+++ components/layouts/main/MainHeaderDesktop.tsx
@@ -0,0 +1,7 @@
+import TextComponent from "@/components/common/text/TextComponent";
+
+function MainHeaderDesktop() {
+    <TextComponent>desktop</TextComponent>
+}
+
+export default MainHeaderDesktop;
